aaccd0376326a7b5a73bae01030368f5190dd4b4
Jameswilliamquinn2016 <jameswilliamquinn@icloud.com>
2017-02-18 22:50:49 +0000

    Added one record

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 var products = [
   {id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100},
   {id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100},
-  {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100}
+  {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100},
+  {id: 4, name: 'Vue', description: 'A JavaScript Library for building user interfaces.', price: 100}
 ];
 
 function findProduct (productId) {
